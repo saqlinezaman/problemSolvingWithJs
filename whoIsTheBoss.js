c61424d27e72e80b487364815034421800f7d1ff
saqlinezaman <saqlinemoaj@gmail.com>
2025-06-18 21:00:30 +0600

    who is the boss

---
+++ whoIsTheBoss.js
@@ -0,0 +1,10 @@
+function boss(a,b,c){
+    if(a>b && a>c){
+        console.log(a ,'is the Bigboss')
+    }else if(b>a && b>c){
+        console.log(b ,'is the Bigboss')
+    }else{
+        console.log(c ,'is the Bigboss')
+    }
+ }
+boss(20,30,45);
